0c31cfd0feb87d008ad56822734da17f7b386959
Cassiano Junior <cassianojuniorww@gmail.com>
2022-10-02 22:16:38 -0300

    :sparkles: Adding special cases: Infinite solutions and ilimited solution

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -1,11 +1,15 @@
 import utils
 
+HAS_SOLUTION = 0
+ILIMITED_SOLUTION = 1
+INFINITE_SOLUTIONS = 2
 
 def execute(objectiveFunction, constraintMatrix, bVector, isMaxProblem = False, needFirstPhase = False):
   if not needFirstPhase:
     return executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase)
 
 def executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase):
+  solutionType = HAS_SOLUTION
   tableau = utils.createTableau(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase)
   utils.showTableau(tableau)
   tableau = utils.toCanonicForm(tableau)
@@ -14,6 +18,12 @@ def executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, ne
   bestSolution, inputVariable = utils.checkOptimality(tableau)
 
   while not bestSolution:
+    isUnbounded = utils.checkUnboundedness(tableau, inputVariable)
+    
+    if isUnbounded:
+      solutionType = ILIMITED_SOLUTION
+      break
+
     outputVariable = utils.getOutputVariable(tableau, inputVariable)
     tableau = utils.pivoting(tableau, inputVariable, outputVariable)
 
@@ -25,4 +35,7 @@ def executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, ne
 
   tableau[len(tableau)-1][len(tableau[0])-1] *= multiplyer
 
-  return tableau
+  isEndless = utils.checkEndless(tableau)
+  if solutionType == HAS_SOLUTION and isEndless: solutionType = INFINITE_SOLUTIONS
+
+  return tableau, solutionType
