b761d53c40a69f4dddf636f04a63308251b6bb27
Cassiano Junior <cassianojuniorww@gmail.com>
2022-10-02 21:28:05 -0300

    :recycle: Refactoring code: pivoting function | :bug: Fixing the negative best solution when the problem is to maximize

--- SimplexMethod.py
+++ SimplexMethod.py
@@ -1,7 +1,11 @@
 import utils
 
 
-def execute(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase):
+def execute(objectiveFunction, constraintMatrix, bVector, isMaxProblem = False, needFirstPhase = False):
+  if not needFirstPhase:
+    return executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase)
+
+def executePhase2(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase):
   tableau = utils.createTableau(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirstPhase)
   utils.showTableau(tableau)
   tableau = utils.toCanonicForm(tableau)
@@ -17,16 +21,8 @@ def execute(objectiveFunction, constraintMatrix, bVector, isMaxProblem, needFirs
 
     bestSolution, inputVariable = utils.checkOptimality(tableau)
   
+  multiplyer = 1 if isMaxProblem else -1
 
-  print("Best solution: Z* = ", -1*tableau[len(tableau)-1][len(tableau[0])-1])
-  print("Base variables: ")
-  baseVariables = utils.getBaseVariables(tableau, utils.getTableauConstraints(tableau))
-  nonBaseVariables = utils.getNonBaseVariables(tableau)
-  for variable in baseVariables:
-    print(f"\tx{variable}* = {tableau[baseVariables.index(variable)+1][len(tableau[0])-1]}")
-  print("Non-base variables: ")
-  for variable in nonBaseVariables:
-    print(f"\tx{variable}* = 0")
+  tableau[len(tableau)-1][len(tableau[0])-1] *= multiplyer
 
-def executePhase2(function, constraints, isMaxProblem):
-  tableau = utils.createTableau(function, constraints, isMaxProblem)
+  return tableau
